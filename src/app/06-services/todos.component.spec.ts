4272bcb36b5f5439485484490df9aee58dd55854
Matthias Moosburger <moosburger@cip.ifi.lmu.de>
2017-03-19 16:20:35 +0100

    test service with spy

--- src/app/06-services/todos.component.spec.ts
+++ src/app/06-services/todos.component.spec.ts
@@ -1,12 +1,26 @@
-import { TodosComponent } from './todos.component'; 
-import { TodoService } from './todo.service'; 
+import { TodosComponent } from './todos.component';
+import { TodoService } from './todo.service';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/from';
 
 describe('TodosComponent', () => {
   let component: TodosComponent;
+  let todoService: TodoService;
 
   beforeEach(() => {
+    todoService = new TodoService(null);
+    component = new TodosComponent(todoService);
   });
 
-  it('', () => {
+  it('should set todos property with items from server', () => {
+    let todos = [1, 2, 3];
+
+    spyOn(todoService, 'getTodos').and.callFake(() => {
+      return Observable.from([todos]);
+    });
+
+    component.ngOnInit();
+
+    expect(component.todos).toBe(todos);
   });
 });
